8824e21830098f6b7204f072bc90e6c906717f57
macctown <macctown@hotmail.com>
2015-09-06 10:27:59 -0400

    Lintcode-Update Longeset Common Subsequence
    Comments for Longeset Common Subsequence

--- src/lintcode/LongestCommonSubsequence.java
+++ src/lintcode/LongestCommonSubsequence.java
@@ -6,27 +6,36 @@ public class LongestCommonSubsequence {
      * @return: The length of longest common subsequence of A and B.
      */
     public static int longestCommonSubsequence(String A, String B) {
-        // write your code here
+        //corner case
     	if(A ==null || B == null){
     		return 0;
     	}
     	
+    	//set length+1 for array-trick
     	int[][] common = new int[A.length()+1][B.length()+1];
     	
+    	//set initial value for first row and column
+    	//common[i][0] means first i letters in A and 0 letters in B, which has no common sub
     	for(int i=0;i<A.length();i++){
     		common[i][0] = 0;
     	}
     	
+    	//set initial value for first row and column
+    	//common[0][i] means first i letters in B and 0 letters in A, which has no common sub
     	for(int i=0;i<B.length();i++){
     		common[0][i] = 0;
     	}
     	
+    	//loop from second letter of A and <=length
     	for(int i=1;i<=A.length();i++){
+    		//loop from second letter of B and <=length
     		for(int j=1;j<=B.length();j++){
+    			//if A,B has same letter in same position, the common equals the last common+1
     			if(A.charAt(i-1)==B.charAt(j-1)){
     				common[i][j] = common[i-1][j-1]+1;
     			}
     			else{
+    				//if A,B isn't same in same position, the common is the max of two elements below
     				common[i][j] = Math.max(common[i-1][j], common[i][j-1]);
     			}
     		}
