f42e05559ee95a31d8aa696415083a5eb557d5cd
macctown <macctown@hotmail.com>
2015-09-22 13:46:54 -0400

    Leetcode-Merge Sorted Array

--- src/leetcode/MergeSortedArray.java
+++ src/leetcode/MergeSortedArray.java
@@ -1,58 +1,51 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class MergeSortedArray {
-	public static void merge(int[] nums1, int m, int[] nums2, int n) {
-		int total = m+n;
-		
-		for(int i=m-1, j=n-1;total>0;total--){
-			System.out.print("array: ");
-			for(int o=0;o<nums1.length;o++){
-				System.out.print(nums1[o]+" ");
-			}
-			
-			System.out.print("i: "+i);
-			System.out.print(" j: "+j);
-			System.out.print(" total: "+total);
-			System.out.println();
-			if(i<0){
-				//put all elements left in nums1 in final nums
-				System.out.print("nums2: "+nums2[j]);
-				nums1[total-1] = nums2[j];
-				j--;
-			}
-			else if(j<0){
-				//put all elements left in nums1 in final nums
-				nums1[total-1] = nums1[i];
-				i--;
-			}
-			else{
-				//compare which one is bigger and put in the back of final nums
-				if(nums1[i] > nums2[j]){
-					nums1[total-1] = nums1[i];
-					i--;
-				}
-				else{
-					nums1[total-1] = nums2[j];
-					j--;
-				}
-			}
-		}
-		
-		
-    }
+	//pre-condition: nums1 has enough space
 	
-	public static void main(String[] args){
-		int[] array1 = new int[10];
-		int[] array2 = new int[3];
-		
-		array2[0] = 1;
-		 
-		
-		merge(array1,0,array2,1);
-		
+	
+	//1.combine, then sort
+	//TC: O(n)+O(sort)
+	
+	
+	//2.put big in the back, loop all the array from back to start, then find who has left and add to res
+	//TC: O(n)+O(k)
+	public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+	       int total = m+n-1;
+	       int len1 = m-1;
+	       int len2 = n-1;
+	       //two pointer move from back to start
+	       while(len1>=0&&len2>=0){
+	    	   //put the bigger one in the back
+	    	   nums1[total--] = (nums1[len1] >= nums2[len2]) ? nums1[len1--] : nums2[len2--];
+	    	   //minus one after use
+	       }
+	       
+	       //check which array has elements left and add to nums1
+	       if(len1>=0){
+	    	   for(int i=len1;i>=0;i--){
+	    		 //minus one after use
+	    		   nums1[total--] = nums1[len1--];
+	    	   }
+	       }
+	       else if(len2>=0){
+	    	   for(int i=len2;i>=0;i--){
+	    		 //minus one after use
+	    		   nums1[total--] = nums2[len2--];
+	    	   }
+	       }
+    	   
+	       
+	       for(int i=0;i<nums1.length;i++){
+	    	  System.out.println(nums1[i]);
+	       }
+	}
+	
+	
+	
+	public static void main(String args[]){
+		int[] arr1 = {1,2,3,0,0,0};
+		int[] arr2 = {4,5,6};
+		merge2(arr1, 3, arr2, 3);
 	}
 }
