6773477a6a0aadefb94d3241527b6e4d66d67764
macctown <macctown@hotmail.com>
2015-09-22 13:46:38 -0400

    Leetcode-ContainsDuplicate

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -1,26 +1,59 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
-	public static boolean containsDuplicate(int[] nums) {
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for(int i=0;i<nums.length;i++)
-    	{
-    		if(map.containsKey(nums[i]))
-    		{
-    			return true;
-    		}
-    		else{
-    			map.put(nums[i], i);
-    		}
-    	}
-		return false;
+	
+	//1. use hashmap store num and its times of appear
+	//TC: O(n)
+	public boolean containsDuplicate(int[] nums) {
+		//corner case
+        if(nums.length==0||nums==null){
+        	return false;
+        }
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int len = nums.length-1;
+        while(len>=0){
+        	int count = 0;
+        	if(!map.containsKey(nums[len])){
+        		map.put(nums[len--], count);
+        	}
+        	else{
+        		return true;
+        	}
+        }
+        return false;
     }
 	
-	public static void main(String[] args){
-		System.out.println("123");
-		int[] nums={3,5,1,2,4,4};
-		System.out.println(containsDuplicate(nums));
+	
+	//use hashset's feature, return false after add duplicate element
+	public static boolean containsDuplicate2(int[] nums) {
+		//corner case
+        if(nums.length==0||nums==null){
+        	return false;
+        }
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        int len = nums.length-1;
+        while(len>=0){
+        	boolean flag = set.add(nums[len--]);
+        	if(flag==false){
+        		return true;
+        	}
+        }
+        return false;
 	}
+	
+	public static void main(String args[]){
+		int[] test1 = {};
+		int[] test2 = {1,2,5,4,5};
+		int[] test3 = {1,1};
+		System.out.println(containsDuplicate2(test1));
+		System.out.println(containsDuplicate2(test2));
+		System.out.println(containsDuplicate2(test3));
+	}
+
+
 }
