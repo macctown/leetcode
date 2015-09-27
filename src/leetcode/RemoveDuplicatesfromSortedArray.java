7b0cca6a343ea698e4981d25328cbda2a94bf018
macctown <macctown@hotmail.com>
2015-09-26 21:13:15 -0400

    Leetcode-Remove Duplicate From RSA

--- src/leetcode/RemoveDuplicatesfromSortedArray.java
+++ src/leetcode/RemoveDuplicatesfromSortedArray.java
@@ -26,10 +26,29 @@ public class RemoveDuplicatesfromSortedArray {
         return newLength;
     }
 	
+		//two poitner, continue when same, when it's differ, just move end and give end the differ value 
+		public static int removeDuplicates2(int[] nums) {
+			//corner case
+			if(nums==null || nums.length==0){
+				return 0;
+			}
+			int cur = 0;
+			for(int i=1;i<nums.length;i++){
+				if(nums[i]!=nums[cur]){
+					cur++;
+					nums[cur] = nums[i];
+				}
+			}
+
+	        return cur+1;
+	    }
+	
+	
+	
 	
 	public static void main(String args[]){
 		int[] test1 = {1,2,2,3,3,3,4};
 		int[] test2 = {9,9};
-		System.out.println(removeDuplicates(test1));                                                                          
+		System.out.println(removeDuplicates2(test1));                                                                          
 	}
 }
